e48715644a1001e3948962812de1032d8e2d4672
abhirajakhare <abhirajakhare@gmail.com>
2023-07-15 10:33:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title ('My Parents New Healthy Diner')
 
+streamlit.header ('Breakfast Menu')
+streamkit.text ('Omega 3 & Blueberry Oatmeal') 
+streamlit.text ('kale, Spanich & Rocket Smothie')
+streamlit.text ( 'Hard-Boiled Free-Range Eggs') 
