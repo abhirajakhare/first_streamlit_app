5a1f331ee9b886d23ed96a8e2b6345dd0742a4f6
abhirajakhare <abhirajakhare@gmail.com>
2023-07-15 14:01:57 -0500

    Update streamlit_app.py
    Added API call to Fruitvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Display The table on the page
 streamlit.dataframe(fruits_to_show)
 
+
+
+# New Section to display Fruitvicd api response 
+import requests 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
