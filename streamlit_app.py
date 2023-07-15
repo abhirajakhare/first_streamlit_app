ee8539efecec4d2564e0155af47aca07cdcbcc53
abhirajakhare <abhirajakhare@gmail.com>
2023-07-15 11:14:38 -0500

    Update streamlit_app.py
    added Fruit Index to data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas as pd
-streamlit.title ('My Parents New Healthy Diner')
+streamlit.title ('My Mom's New Healthy Diner')
 
 streamlit.header ('Breakfast Menu')
 streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal') 
@@ -10,8 +10,11 @@ streamlit.text ('🥑🍞 Avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv ('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt') 
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 #Fruit multiselect 
 streamlit.multiselect ( "Pick Some Fruits:", list(my_fruit_list.index))
+
 #Display The table on the page
 streamlit.dataframe(my_fruit_list)
 
