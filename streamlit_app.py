b9fd956d8f2b7f0443a58bbca343aaa083d9653f
abhirajakhare <abhirajakhare@gmail.com>
2023-07-15 14:09:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas as pd
+import pandas
 streamlit.title ('My Mom\'s New Healthy Diner')
 
 streamlit.header ('Breakfast Menu')
@@ -9,7 +9,7 @@ streamlit.text ('🐔 Hard-Boiled Free-Range Eggs')
 streamlit.text ('🥑🍞 Avocado toast') 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pd.read_csv ('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt') 
+my_fruit_list = pandas.read_csv ('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt') 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Fruit multiselect 
