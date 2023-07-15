6fdc9b3c4a506b8d7c2779963e71cfe4b6afc691
abhirajakhare <abhirajakhare@gmail.com>
2023-07-15 10:38:10 -0500

    Update streamlit_app.py
    Added Avocado Toast & new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 streamlit.title ('My Parents New Healthy Diner')
 
 streamlit.header ('Breakfast Menu')
-streamlit.text ('Omega 3 & Blueberry Oatmeal') 
-streamlit.text ('kale, Spanich & Rocket Smothie')
-streamlit.text ( 'Hard-Boiled Free-Range Eggs') 
+streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal') 
+streamlit.text ('🥗 kale, Spanich & Rocket Smothie')
+streamlit.text ('🐔 Hard-Boiled Free-Range Eggs') 
+streamlit.text ('🥑🍞 Avocado toast') 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
